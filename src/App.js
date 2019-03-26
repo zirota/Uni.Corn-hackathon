8e58b27630b0b8c173f631f13da9b8c47820d433
lungkuangkoh <lungkuang.k@gmail.com>
2019-03-26 10:48:23 +0800

    'Commit'

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        {/* <Route exact path="/" component={Home}></Route>
+        <Route exact path="/" component={Savings}></Route>
+        <Route exact path="/" component={}></Route>
+        <Route exact path="/" component={Home}></Route> */}
+      </div>
+    );
+  }
+}
+
+export default App;
