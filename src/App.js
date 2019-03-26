4c1ca1b8514b488f30660b4a2bccf4019d51d9f7
Ayush Chattoraj <ayush.chattoraj@gmail.com>
2019-03-26 11:25:44 +0800

    Add basic API functionality

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import axios from 'axios';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {json: null};
+
+    let url = "http://api-gateway-dbs-techtrek.ap-southeast-1.elasticbeanstalk.com/customers/marytan";
+
+    let myjson = axios.get(url, {headers: {'identity': 'Group14', 'token': '9f0ffe48-1e61-47d5-804f-205e769c7fde'}}).then(res => {
+      this.setState({json: res});
+    });
+
+  }
+
+
   render() {
+    console.log(this.state);
     return (
       <div className="App">
-        {/* <Route exact path="/" component={Home}></Route>
-        <Route exact path="/" component={Savings}></Route>
-        <Route exact path="/" component={}></Route>
-        <Route exact path="/" component={Home}></Route> */}
+        <p> Hello mate </p>
       </div>
     );
   }
