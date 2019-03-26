7a097918716d1897c03735d8a7bb165b900e55d4
lungkuangkoh <lungkuang.k@gmail.com>
2019-03-26 11:22:11 +0800

    'Home'

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import Home from "./components/Home";
+// import Savings from "./components/Savings";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        {/* <Route exact path="/" component={Home}></Route>
-        <Route exact path="/" component={Savings}></Route>
-        <Route exact path="/" component={}></Route>
-        <Route exact path="/" component={Home}></Route> */}
-      </div>
+      <Router>
+        <div className="App">
+          <Route exact path="/" component={Home} />
+          {/* <Route exact path="/Savings" component={Savings}> */}
+        </div>
+      </Router>
     );
   }
 }
